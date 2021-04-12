b1bfd2d994e35cd77ba2e64eb105a9fc75486cde
xeonqq <xeonqq@gmail.com>
2021-04-12 20:36:41 +0200

    Update model to have two additional labels

--- src/speaker.py
+++ src/speaker.py
@@ -14,15 +14,25 @@ class Speaker(object):
     def __init__(self):
         pygame.mixer.init()
         self._last_played_time = 0
+        self._sound_tracks = {
+                "visibility_clear": pygame.mixer.Sound("./sounds/red-alert/visibility-clear.mp3"),
+                "1-up": pygame.mixer.Sound("./sounds/mario/smb_1-up.wav"),
+                "green": pygame.mixer.Sound("./sounds/wife/green.wav"),
+                "red": pygame.mixer.Sound("./sounds/wife/red.wav"),
+                "yellow": pygame.mixer.Sound("./sounds/wife/yellow.wav"),
+                "red_yellow": pygame.mixer.Sound("./sounds/wife/red-yellow.wav"),
+            }
         self._sound_map = {
-            "ready": pygame.mixer.Sound("./sounds/red-alert/visibility-clear.mp3"),
-            "traffic": pygame.mixer.Sound("./sounds/mario/smb_1-up.wav"),
-            "green": pygame.mixer.Sound("./sounds/wife/green.wav"),
-            "red": pygame.mixer.Sound("./sounds/wife/red.wav"),
-            "yellow": pygame.mixer.Sound("./sounds/wife/yellow.wav"),
-            "red_yellow": pygame.mixer.Sound("./sounds/wife/red-yellow.wav"),
-            "green_left": pygame.mixer.Sound("./sounds/wife/green.wav"),
-            "red_left": pygame.mixer.Sound("./sounds/wife/red.wav"),
+            "ready": "visibility_clear",
+            "traffic": "1-up",
+            "green": "green",
+            "red": "red",
+            "yellow": "yellow",
+            "red_yellow": "red_yellow",
+            "green_left": "green",
+            "red_left": "red",
+            "green_right": "green",
+            "red_right": "red",
             "side": None,
             "none": None,
         }
@@ -34,7 +44,10 @@ class Speaker(object):
 
         current_time = time.time()
         if (key != self._prev_sound_key) or (current_time - self._last_played_time> 8):
-            sound = self._sound_map.get(key, None)
+            sound_track_name = self._sound_map.get(key, None)
+            sound = self._sound_tracks.get(sound_track_name, None)
+            if not sound:
+                return
             channel = sound.play()
             self._prev_sound_key = key
             self._last_played_time = current_time
