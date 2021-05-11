8ddc9e0c25479cc275f63edf8ac52d51107f57a3
xeonqq <xeonqq@gmail.com>
2021-05-11 13:14:57 +0200

    Update models

--- linux_run.sh
+++ linux_run.sh
@@ -2,4 +2,4 @@
 xhost +SI:localuser:root
 docker run --rm -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=$DISPLAY   \
--it -p 5005:5005 -v "$(pwd)":/workspace  -v /media/qq/5256C31056C2F3AF5/pi_cam_record/picam2/:/mnt/hdd --privileged -v /dev/bus/usb:/dev/bus/usb co-pilot:latest /bin/bash
+-it -p 5005:5005 -v "$(pwd)":/workspace  -v /media/qq/5256C31056C2F3AF6/pi_cam_record/picam2/:/mnt/hdd --privileged -v /dev/bus/usb:/dev/bus/usb co-pilot:latest /bin/bash
