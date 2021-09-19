f57c7e2fb2350fd0ff84ebd23c95326ced18c28d
xeonqq <xeonqq@gmail.com>
2021-09-19 16:20:48 +0200

    Refactor to use capture_continuous function

--- src/camera_capturer.py
+++ src/camera_capturer.py
@@ -24,27 +24,22 @@ class CameraCapturer(object):
         self._thread.start()
 
     def _run(self):
-        while True:
-            if self._is_recording_query_func():
+        for _ in self._camera.capture_continuous(
+                self._stream,
+                format="rgb",
+                resize=self._capture_resolution,
+                use_video_port=True):
+            # start = time.perf_counter()
+            self._stream.truncate()
+            self._stream.seek(0)
+            img = Image.frombuffer(
+                "RGB", self._capture_resolution, self._stream.getvalue()
+            )
 
-                start = time.perf_counter()
-                # 80-150ms
-                self._camera.capture(
-                    self._stream,
-                    format="rgb",
-                    resize=self._capture_resolution,
-                    use_video_port=True,
-                )
-                self._stream.truncate()
-                self._stream.seek(0)
-                img = Image.frombuffer(
-                    "RGB", self._capture_resolution, self._stream.getvalue()
-                )
-
-                current_time = time.perf_counter()
-                logging.debug("capture at {}".format(current_time))
-                # print("capture 1: {} ms".format((time.perf_counter() - start)*1000))
-                self._pubsub.publish((img, current_time))
-                logging.debug("exposure_speed:{}".format(self._camera.exposure_speed))
-                # print("capture 2: {} ms".format((time.perf_counter() - start)*1000))
-                time.sleep(self._dt)
+            current_time = time.perf_counter()
+            logging.debug("capture at {}".format(current_time))
+            # print("capture 1: {} ms".format((time.perf_counter() - start)*1000))
+            self._pubsub.publish((img, current_time))
+            logging.debug("exposure_speed:{}".format(self._camera.exposure_speed))
+            # print("capture 2: {} ms".format((time.perf_counter() - start)*1000))
+            time.sleep(self._dt)
