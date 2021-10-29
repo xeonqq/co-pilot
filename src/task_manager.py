9f4d9c1821e38028b771eb0454ab3c0d96c5a0fe
xeonqq <xeonqq@gmail.com>
2021-10-29 22:55:06 +0200

    Use default blackbox path

--- src/task_manager.py
+++ src/task_manager.py
@@ -23,15 +23,13 @@ class ProcessTask(object):
 class CoPilotTask(ProcessTask):
     def __init__(self):
         ProcessTask.__init__(self, "SwitchTaskEvent")
-        self._cmd = """python3 -m src.main  --ssd_model models/ssd_mobilenet_v2_coco_quant_no_nms_edgetpu.tflite  --label models/coco_labels.txt --score_threshold 0.3 --traffic_light_classification_model models/traffic_light_edgetpu.tflite  --traffic_light_label models/traffic_light_labels.txt --blackbox_path /tmp"""
+        self._cmd = """python3 -m src.main  --ssd_model models/ssd_mobilenet_v2_coco_quant_no_nms_edgetpu.tflite  --label models/coco_labels.txt --score_threshold 0.3 --traffic_light_classification_model models/traffic_light_edgetpu.tflite  --traffic_light_label models/traffic_light_labels.txt"""
 
 
 class DashCamTask(ProcessTask):
     def __init__(self):
         ProcessTask.__init__(self, "DashCamTask")
-        self._cmd = (
-            """python3 -m src.dashcam --record_on_motion --blackbox_path=/home/pi"""
-        )
+        self._cmd = """python3 -m src.dashcam --record_on_motion"""
 
 
 class SwitchTaskEvent(object):
