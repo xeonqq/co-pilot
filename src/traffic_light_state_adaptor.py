a198a7bccf590188f5f7fb30c7a79525199529b2
xeonqq <xeonqq@gmail.com>
2021-11-25 21:09:18 +0100

    Do not alert red when previous state is yellow

--- src/traffic_light_state_adaptor.py
+++ src/traffic_light_state_adaptor.py
@@ -3,7 +3,7 @@ from .utils import is_green, is_red
 
 
 class TrafficLightStateAdaptor(object):
-    def __init__(self):
+    def __init__(self, is_full_mode):
         self._prev_state = None
         self._state_to_sound_map = {
             "green": "green",
@@ -18,6 +18,7 @@ class TrafficLightStateAdaptor(object):
             "none": None,
         }
         self._last_update_time = 0
+        self._is_full_mode = is_full_mode
 
     def update(self, state):
         sound_track = None
@@ -29,8 +30,10 @@ class TrafficLightStateAdaptor(object):
         # do not distinguish left right
         traffic_light_state = self._state_to_sound_map.get(state, None)
         if (
-            traffic_light_state != self._prev_state
-        ) and not self._was_red_yellow_now_green(traffic_light_state):
+            (traffic_light_state != self._prev_state)
+            and not self._was_red_yellow_now_green(traffic_light_state)
+            and not self._was_yellow_now_red(traffic_light_state)
+        ):
             sound_track = traffic_light_state
 
         self._prev_state = traffic_light_state
@@ -43,3 +46,6 @@ class TrafficLightStateAdaptor(object):
 
     def _was_red_yellow_now_green(self, traffic_light_state):
         return self._prev_state == "red_yellow" and is_green(traffic_light_state)
+
+    def _was_yellow_now_red(self, traffic_light_state):
+        return self._prev_state == "yellow" and is_red(traffic_light_state)
