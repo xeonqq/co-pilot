70c4a58918d09b7ae6c2722e2401b2f8a86ebd54
xeonqq <xeonqq@gmail.com>
2021-12-05 22:12:21 +0100

    Revert "Adjust wav volume by 8 dB"
    This reverts commit bab63e9a0d3c6f66e6f34d35147edd56bc2cb2ae.

--- sounds/adjust_wav_volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "increase all wav file volume by 8 dB"
-shopt -s globstar
-output="/tmp/output_louder.wav"
-for i in **/*.wav; do # Whitespace-safe and recursive
-    ffmpeg -y -i "$i" -filter:a volume=8dB "$output"
-    mv "$output" "$i"
-done
